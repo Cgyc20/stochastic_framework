0819eed9194b768d4473e5531636cdfbb2b47e39
Cgyc20 <cgyc20@bath.ac.uk>
2025-10-29 15:15:11 +0000

    Update reaction parameters in demo to reflect correct stoichiometry

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from stochastic_framework import Reaction
 
 Model = Reaction()
 
-Model.add_reaction({"A": 1}, {"B": 1}, 0.5)
+Model.add_reaction({"A": 2}, {"B": 1}, 0.5)
 Model.add_reaction({"B": 1}, {"A": 1}, 0.6)
 Model.add_reaction({},{"A":1},1.0)
 Model.add_reaction({"B":1},{},0.1)
